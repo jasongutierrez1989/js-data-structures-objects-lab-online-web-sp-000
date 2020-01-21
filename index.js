4a4a397d61f5e8876360de7cbd6681e7e14e7324
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2020-01-21 04:18:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+// Write your solution in this 
+const driver = {};
+
+function updateDriverWithKeyAndValue(driver, key, value){
+  const newDriver = driver;
+  newDriver.assign(key: value);
+  return newDriver;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+  
+}
+
+function deleteFromDriverByKey(driver, key){
+  
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key){
+  
+}
